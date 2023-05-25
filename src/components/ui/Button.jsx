469763064b4e9338b15cc68c7670603099288f6d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-25 22:22:57 +0200

    Add basic ui components: Button, TextInput

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,5 @@
+import { Button as UiButton } from "@chakra-ui/react";
+
+export const Button = () => {
+  return <UiButton>UiButton</UiButton>;
+};
---
+++ src/components/ui/TextInput.jsx
@@ -0,0 +1,5 @@
+import { Imput } from "@chakra-ui/react";
+
+export const TextImput = () => {
+  return <Imput>Imput</Imput>;
+};
