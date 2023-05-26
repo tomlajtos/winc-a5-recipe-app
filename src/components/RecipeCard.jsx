cc499c4f8b2560f951235b5134a8d0e9a8b0898f
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-26 22:21:14 +0200

    Add code to show recipe data

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,45 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Tag,
+  Highlight,
+  Text,
+  Heading,
+} from "@chakra-ui/react";
+import { fixLabel } from "../utils/utils.js";
 
-export const RecipeCard = () => {
+export const RecipeCard = ({ recipe, handleClick }) => {
   return (
-    <Container>
-      <div>Recipe Card</div>
-    </Container>
+    <Card
+      p={0}
+      maxW={350}
+      borderRadius={"lg"}
+      onClick={() => handleClick(recipe)}
+    >
+      <CardHeader p={0} maxH={"250px"} overflow={"hidden"}>
+        <Image borderTopRadius={"lg"} src={recipe.image} />
+      </CardHeader>
+      <CardBody>
+        <Heading pb={6} size={"lg"} textAlign={"center"}>
+          {fixLabel(recipe.label)}
+        </Heading>
+        {recipe.mealType.map((type, index) => (
+          <Text key={index}>{type}</Text>
+        ))}
+        <hr />
+        {recipe.dishType.map((type, index) => (
+          <Text key={index}>{type}</Text>
+        ))}
+        <hr />
+        {recipe.cautions.map((type, index) => (
+          <Text key={index}>{type}</Text>
+        ))}
+        <hr />
+      </CardBody>
+      <CardFooter></CardFooter>
+    </Card>
   );
 };
