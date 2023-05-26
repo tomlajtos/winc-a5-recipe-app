652b3e83e5346c10e649f8eebd0d513ead16010d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-26 22:23:35 +0200

    Add "RecipeList" component with minimal code

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react";
+import { RecipeCard } from "./RecipeCard";
+
+export const RecipeList = ({ recipes }) => {
+  return (
+    <Container>
+      <div>Recipe List</div>
+      <RecipeCard recipe={recipes[19]} />
+    </Container>
+  );
+};
