c038353a48fa530f0a526268acd1c2afe0dbb5c6
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-27 22:41:09 +0200

    refactor(RecipeList): Change "Container" to "Wrap"

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,11 +1,12 @@
-import { Container } from "@chakra-ui/react";
+import { Wrap } from "@chakra-ui/react";
 import { RecipeCard } from "./RecipeCard";
 
 export const RecipeList = ({ recipes }) => {
   return (
-    <Container>
-      <div>Recipe List</div>
-      <RecipeCard recipe={recipes[7]} />
-    </Container>
+    <Wrap justify={"center"} spacing={4} p={4}>
+      {recipes.map((recipe, index) => (
+        <RecipeCard key={index} recipe={recipe} />
+      ))}
+    </Wrap>
   );
 };
