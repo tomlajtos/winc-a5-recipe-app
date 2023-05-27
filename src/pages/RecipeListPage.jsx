c40cfe7abc721d2aeaf717438d7fa1ae1c778db6
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-27 22:52:22 +0200

    refactor(RecipeListPage): remove RecipeList, change title and styles

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,18 +1,15 @@
 import { Center, Heading } from "@chakra-ui/react";
 import { RecipeSearch } from "../components/RecipeSearch";
-import { RecipeList } from "../components/RecipeList";
 import { data } from "../utils/data";
 
 export const RecipeListPage = () => {
   const recipes = data.hits.map((hit) => hit.recipe);
   // console.log(recipes[7]);
-  console.log(recipes);
-
+  // console.log(recipes);
   return (
-    <Center h="100vh" flexDir="column">
-      <Heading>Your Recipe App</Heading>
-      <RecipeSearch />
-      <RecipeList recipes={recipes} />
+    <Center minH="100vh" flexDir="column">
+      <Heading>Recipe App</Heading>
+      <RecipeSearch recipes={recipes} />
     </Center>
   );
 };
