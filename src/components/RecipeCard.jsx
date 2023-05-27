f4812f286f6f3fd28380f21be8ea78d2728aea72
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-27 22:39:38 +0200

    refactor(RecipeCard): Add helper functions, cond rendering

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -16,7 +16,7 @@ import {
  * Each recipe obj. in the db-file represents a single recipe,
  * so the word "recipes" in some of the labels does not make any sense.
  */
-export const fixLabel = (label) => {
+const fixLabel = (label) => {
   const words = label.toLowerCase().split(" ");
   if (words.includes("recipes")) {
     return words
@@ -28,32 +28,73 @@ export const fixLabel = (label) => {
   }
 };
 
+const showLabels = (labels, isHealthLabel = false) => {
+  if (labels.length > 0) {
+    return isHealthLabel
+      ? labels
+          .filter(
+            (label) =>
+              label.toLowerCase() === "vegan" ||
+              label.toLowerCase() === "vegetarian"
+          )
+          .map((label, index) => (
+            <Text bg={"green.100"} key={index}>
+              {label}
+            </Text>
+          ))
+      : labels.map((label, index) => (
+          <Text bg={"orange.100"} key={index}>
+            {label}
+          </Text>
+        ));
+  }
+};
+
+const showTypes = (types) => {
+  if (types.length > 0) {
+    return types.map((type, index) => (
+      <Text bg={"gray.100"} key={index}>
+        {type}
+      </Text>
+    ));
+  }
+};
+
 export const RecipeCard = ({ recipe, handleClick }) => {
+  const {
+    image,
+    label,
+    mealType,
+    dishType,
+    healthLabels,
+    dietLabels,
+    cautions,
+  } = recipe;
+
   return (
-    <Card
-      p={0}
-      maxW={350}
-      borderRadius={"lg"}
-      onClick={() => handleClick(recipe)}
-    >
-      <CardHeader p={0} maxH={"250px"} overflow={"hidden"}>
-        <Image borderTopRadius={"lg"} src={recipe.image} />
+    <Card p={0} w={350} borderRadius={"lg"} onClick={() => handleClick(recipe)}>
+      <CardHeader p={0} h={"250px"} overflow={"hidden"}>
+        <Image
+          w={350}
+          borderTopRadius={"lg"}
+          src={image}
+          alt={`image of ${label}`}
+        />
       </CardHeader>
       <CardBody>
         <Heading pb={6} size={"lg"} textAlign={"center"}>
-          {fixLabel(recipe.label)}
+          {fixLabel(label)}
         </Heading>
-        {recipe.mealType.map((type, index) => (
-          <Text key={index}>{type}</Text>
-        ))}
-        <hr />
-        {recipe.dishType.map((type, index) => (
-          <Text key={index}>{type}</Text>
-        ))}
-        <hr />
-        {recipe.cautions.map((type, index) => (
-          <Text key={index}>{type}</Text>
-        ))}
+        {showTypes(mealType)}
+        {showTypes(dishType)}
+        {showLabels(healthLabels, true)}
+        {showLabels(dietLabels)}
+        {cautions.length > 0 &&
+          cautions.map((type, index) => (
+            <Text bg={"red.100"} key={index}>
+              {type}
+            </Text>
+          ))}
         <hr />
       </CardBody>
       <CardFooter></CardFooter>
