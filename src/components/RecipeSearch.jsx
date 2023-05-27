2780add7d59398b85f6d392615ef14afe29ec1aa
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-27 22:44:41 +0200

    feat: Add search in name, move R.List inside R.Search

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,11 +1,26 @@
-import { Container } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { RecipeList } from "./RecipeList";
 import { TextInput } from "./ui/TextInput";
-export const RecipeSearch = () => {
+import { useState } from "react";
+
+export const RecipeSearch = ({ recipes }) => {
+  const [searchField, setSearchField] = useState("");
+
+  const matchedRecipes = recipes.filter((recipe) => {
+    return recipe.label.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  const handleChange = (event) => setSearchField(event.target.value);
+
   return (
-    <Container>
-      <div>Recipe Search</div>
-      <TextInput />
-    </Container>
+    <Flex direction={"column"} align={"center"} gap={8} minW={"100%"} py={8}>
+      <TextInput handleChange={handleChange} />
+
+      {searchField ? (
+        <RecipeList recipes={matchedRecipes} />
+      ) : (
+        <RecipeList recipes={recipes} />
+      )}
+    </Flex>
   );
 };
