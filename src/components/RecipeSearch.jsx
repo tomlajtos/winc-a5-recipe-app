145e071d730aebc39b234786b104e829e160d09e
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-29 23:28:40 +0200

    feat: search in (and filter by) name and/or  health labels

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -5,22 +5,34 @@ import { useState } from "react";
 
 export const RecipeSearch = ({ recipes }) => {
   const [searchField, setSearchField] = useState("");
+  // split user input in serchfield by below special charakters and white-space,
+  // hyphen is not included since it can be found in health labels.
+  const searchTerms = searchField.split(/[\s\+\,\/\\]+/g);
 
-  const matchedRecipes = recipes.filter((recipe) => {
-    return recipe.label.toLowerCase().includes(searchField.toLowerCase());
-  });
+  // helper functions to check for matches in recipe label(title) and health labels
+  const isMatchingLabel = (recipe, term) =>
+    recipe.label.toLowerCase().includes(term.toLowerCase());
+
+  const isMatchingHealthLabel = (recipe, term) =>
+    recipe.healthLabels.join(" ").toLowerCase().includes(term.toLowerCase());
+
+  const findMatchingRecipes = (recipes, terms = []) => {
+    let matchedRecipes = recipes;
+    terms.map((term) => {
+      matchedRecipes = matchedRecipes.filter(
+        (recipe) =>
+          isMatchingLabel(recipe, term) || isMatchingHealthLabel(recipe, term)
+      );
+    });
+    return matchedRecipes;
+  };
 
   const handleChange = (event) => setSearchField(event.target.value);
 
   return (
     <Flex direction={"column"} align={"center"} gap={8} minW={"100%"} py={8}>
       <TextInput handleChange={handleChange} />
-
-      {searchField ? (
-        <RecipeList recipes={matchedRecipes} />
-      ) : (
-        <RecipeList recipes={recipes} />
-      )}
+      <RecipeList recipes={findMatchingRecipes(recipes, searchTerms)} />
     </Flex>
   );
 };
