3ac36c4aeb16df182579b25f83189fb3dd816bc0
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-30 23:10:18 +0200

    Add props and event handling

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,9 @@
 import { Button as UiButton } from "@chakra-ui/react";
 
-export const Button = () => {
-  return <UiButton>UiButton</UiButton>;
+export const Button = ({ handleClick, text, ...props }) => {
+  return (
+    <UiButton onClick={handleClick} {...props}>
+      {text}
+    </UiButton>
+  );
 };
