d79a35b565341c2e63ab42a430bcdd3ba5b8fe98
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-01 22:50:46 +0200

    WIP: Add filterRecipeInfo and showInfoTags experimental functions

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -8,6 +8,7 @@ import {
   Highlight,
   Text,
   Heading,
+  Center,
 } from "@chakra-ui/react";
 import {
   fixLabel,
@@ -50,15 +51,6 @@ const showInfoLabels = function (labels, prefix, selectiveLabels) {
     return labels
       .filter((label) => selectiveLabels.includes(label))
       .map((label, index) => {
-        // console.log(
-        //   "INFO:",
-        //   <Text
-        //     bg={"purple.100"}
-        //     key={betterKeyThenIndex(prefix, label, index)}
-        //   >
-        //     {label}
-        //   </Text>
-        // );
         return (
           <Text
             bg={"purple.100"}
@@ -78,6 +70,63 @@ const showInfoLabels = function (labels, prefix, selectiveLabels) {
     return;
   }
 };
+//=================================================================
+const filterRecipeInfo = function (info, specifiedInfo) {
+  let isInfo;
+  !info // this checks for falsy values (Importantely, resolves null as a velue beofre it would get to .length method)
+    ? (isInfo = false)
+    : (isInfo = Object.keys(info).length); //this is enough for both array and object values
+
+  if (isInfo) {
+    if ("number" === typeof info || "string" === typeof info) {
+      // console.log("I", info);
+      return info;
+    } else if (Array.isArray(info)) {
+      if (specifiedInfo) {
+        // console.log("A-sI:", specifiedInfo, "I:", info);
+        return specifiedInfo.filter((sI) => info.includes(sI));
+      } else {
+        // console.log("A-I:", info);
+        return info;
+      }
+    } else {
+      //if info is an object
+      if (specifiedInfo) {
+        // console.log("O-sI:", specifiedInfo, "I:", info, "types:", infoTypes);
+        return specifiedInfo
+          .filter((sI) => Object.keys(info).includes(sI))
+          .map((sI) => info[sI]);
+      } else {
+        // console.log("O-I:", info);
+        return info;
+      }
+    }
+  }
+  return;
+  // if (Object.prototype.isPrototypeOf(info))
+  // if ("object" === typeof info)
+};
+
+const showInfoTags = function (
+  filteredInfo,
+  prefix,
+  color = "gray.100",
+  textColor = "gray.900",
+  textTransform = "none"
+) {
+  if (filteredInfo) {
+    return filteredInfo.map((inf, index) => (
+      <Tag
+        key={betterKeyThenIndex(prefix, inf, index)}
+        bg={color}
+        textColor={textColor}
+        textTransform={textTransform}
+      >
+        {inf}
+      </Tag>
+    ));
+  }
+};
 // END OF EXPERIMENTAL
 
 const showTypes = (types) => {
@@ -93,7 +142,7 @@ const showTypes = (types) => {
 export const RecipeCard = ({ recipe, handleClick }) => {
   const {
     image,
-    label,
+    label: title,
     mealType,
     dishType,
     healthLabels,
@@ -101,6 +150,8 @@ export const RecipeCard = ({ recipe, handleClick }) => {
     cautions,
   } = recipe;
 
+  // console.log(filterRecipeInfo(recipe.totalNutrients, ["CHOLE", "FAT"]));
+  // console.log("Recipe:", recipe);
   return (
     <Card p={0} w={350} borderRadius={"lg"} onClick={() => handleClick(recipe)}>
       <CardHeader p={0} h={"250px"} overflow={"hidden"}>
@@ -108,29 +159,47 @@ export const RecipeCard = ({ recipe, handleClick }) => {
           w={350}
           borderTopRadius={"lg"}
           src={image}
-          alt={`image of ${label}`}
+          alt={`image of ${title}`}
         />
       </CardHeader>
       <CardBody>
         <Heading pb={6} size={"lg"} textAlign={"center"}>
-          {fixLabel(label)}
+          {fixLabel(title)}
         </Heading>
-        {showTypes(mealType)}
-        {showInfoLabels(mealType, "mt")}
+        <Center>
+          {showInfoTags(
+            filterRecipeInfo(mealType),
+            generateKeyPrefix("mt_", title),
+            "white",
+            "gray.700",
+            "uppercase"
+          )}
+        </Center>
         {showTypes(dishType)}
+        {showInfoTags(
+          filterRecipeInfo(dishType),
+          generateKeyPrefix("dt_", title)
+        )}
         {showLabels(healthLabels, true)}
+        {showInfoTags(
+          filterRecipeInfo(healthLabels, ["Vegetarian", "Vegan"]),
+          generateKeyPrefix("hl_", title)
+        )}
         {showLabels(dietLabels)}
-        {showInfoLabels(healthLabels, generateKeyPrefix("hl_", label), [
-          "Vegan",
-          "Vegetarian",
-          "Keto",
-        ])}
+        {showInfoTags(
+          filterRecipeInfo(dietLabels),
+          generateKeyPrefix("dl_", title)
+        )}
         {cautions.length > 0 &&
           cautions.map((type, index) => (
             <Text bg={"red.100"} key={index}>
               {type}
             </Text>
           ))}
+        {showInfoTags(
+          filterRecipeInfo(cautions),
+          generateKeyPrefix("cau_", title)
+        )}
         <hr />
       </CardBody>
       <CardFooter></CardFooter>
