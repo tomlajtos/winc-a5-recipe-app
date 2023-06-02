e94832dec511834752fbd6804a1c688a99a90e85
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-02 23:44:38 +0200

    Remove comments

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -5,14 +5,6 @@ export const RecipeList = ({ recipes, handleClick }) => {
   return (
     <Wrap justify={"center"} spacing={4} p={4}>
       {recipes.map((recipe) => {
-        // console.log(
-        //   "CARD:",
-        //   <RecipeCard
-        //     key={recipe.label}
-        //     recipe={recipe}
-        //     handleClick={handleClick}
-        //   />
-        // );
         return (
           <RecipeCard
             key={recipe.label}
