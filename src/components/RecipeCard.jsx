2c5cb49e54f66aa90851246fd82866f7e380dda6
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-04 22:27:23 +0200

    refactor/fix: Remove unused code and unecesary key props

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,15 +1,11 @@
 // TODO: move exp funtions to globalFunctions
 import {
   Center,
-  Wrap,
   WrapItem,
   Card,
   CardHeader,
   CardBody,
   Image,
-  Tag,
-  TagLabel,
-  TagLeftIcon,
   Text,
   Heading,
   Stack,
@@ -19,73 +15,11 @@ import { TbPlant2 } from "react-icons/tb";
 import { TbExclamationCircle } from "react-icons/tb";
 import {
   fixLabel,
-  betterKeyThenIndex,
   generateKeyPrefix,
-  checkInput,
+  filterRecipeInfo,
 } from "../utils/globalFunctions";
 import { RecipeInfoTagGroup } from "./RecipeInfoTagGroup";
 
-//EXPERIMENTAL
-const filterRecipeInfo = function (info, specifiedInfo) {
-  if (checkInput(info)) {
-    if ("number" === typeof info || "string" === typeof info) {
-      // console.log("I", info);
-      return [info];
-    } else if (Array.isArray(info)) {
-      if (specifiedInfo) {
-        // console.log("A-sI:", specifiedInfo, "I:", info);
-        return specifiedInfo.filter((sI) => info.includes(sI));
-      } else {
-        // console.log("A-I:", info);
-        return info;
-      }
-    } else {
-      //if info is an object
-      if (specifiedInfo) {
-        // console.log("O-sI:", specifiedInfo, "I:", info, "types:", infoTypes);
-        return specifiedInfo
-          .filter((sI) => Object.keys(info).includes(sI))
-          .map((sI) => info[sI]);
-      } else {
-        // console.log("O-I:", info);
-        return info;
-      }
-    }
-  }
-  return;
-};
-
-const showInfoTags = function (
-  filteredInfo,
-  prefix,
-  textTransform = "none",
-  colorScheme,
-  icon,
-  fontSize,
-  textColor
-) {
-  if (checkInput(filteredInfo)) {
-    return (
-      <Wrap justify={"center"}>
-        {filteredInfo.map((inf, index) => (
-          <WrapItem key={betterKeyThenIndex(prefix, inf, index)}>
-            <Tag
-              fontWeight={600}
-              textTransform={textTransform}
-              colorScheme={colorScheme}
-              fontSize={fontSize}
-              textColor={textColor}
-            >
-              <TagLeftIcon as={icon} boxSize={5} />
-              <TagLabel>{inf}</TagLabel>
-            </Tag>
-          </WrapItem>
-        ))}
-      </Wrap>
-    );
-  }
-};
-// END OF EXPERIMENTAL
 export const RecipeCard = ({ recipe, handleClick }) => {
   const {
     image,
@@ -135,7 +69,6 @@ export const RecipeCard = ({ recipe, handleClick }) => {
           <Stack direction={"column"} justify={"center"} h={"fit-content"}>
             <Center justify={"center"}>
               <Text
-                key={generateKeyPrefix("dt_", title)}
                 fontWeight={600}
                 textColor={"gray.500"}
                 textTransform={"uppercase"}
@@ -145,10 +78,7 @@ export const RecipeCard = ({ recipe, handleClick }) => {
             </Center>
             <Center justify={"center"}>
               <Text justifyContent={"left"}>Dish:</Text>
-              <Text
-                key={generateKeyPrefix("dt_", title)}
-                textTransform={"capitalize"}
-              >
+              <Text textTransform={"capitalize"}>
                 {filterRecipeInfo(dishType)}
               </Text>
             </Center>
