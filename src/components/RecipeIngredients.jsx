6ee1f3559a6d9bd8011174a3aedadd416a5275e8
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-06 23:19:10 +0200

    Add component to display ingredients

---
+++ src/components/RecipeIngredients.jsx
@@ -0,0 +1,27 @@
+import { Flex, Heading, List, ListItem } from "@chakra-ui/react";
+import { betterKeyThenIndex } from "../utils/globalFunctions";
+
+export const RecipeIngredients = ({ ingredients }) => {
+  const formatText = function (text) {
+    return text
+      .match(/[^\*]/g) //removes "*" from begining if any
+      .filter((_char, index, str) => str[index + 1] !== ";") //removes "space" from before ";"
+      .join("");
+  };
+
+  return (
+    <Flex flexDir="column" w={350} alignItems="start" rowGap={2}>
+      <Heading size="md">Ingredients</Heading>
+      <List listStyleType={"none"} mx={0}>
+        {ingredients.map((line, index) => (
+          <ListItem
+            key={betterKeyThenIndex("ingr_", line, index)}
+            textAlign={"left"}
+          >
+            {formatText(line)}
+          </ListItem>
+        ))}
+      </List>
+    </Flex>
+  );
+};
