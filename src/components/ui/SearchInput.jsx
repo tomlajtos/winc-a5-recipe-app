914093f5255f475ac4d536924c0b6679c9514a5a
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-10 23:45:39 +0200

    fix(searchInput): Adjust responsible styles

--- src/components/ui/SearchInput.jsx
+++ src/components/ui/SearchInput.jsx
@@ -5,10 +5,11 @@ export const SearchInput = ({ handleChange }) => {
     <Input
       type="search"
       variant={"outline"}
+      mx={[1, 2, 4]}
       colorScheme="facebook"
       borderColor={"gray.400"}
       focusBorderColor={"purple.800"}
-      maxW={["360px", "80%", "700px"]}
+      maxW={["95%", "90%", "700px"]}
       placeholder="Find a recipe..."
       _placeholder={{ color: "gray.500" }}
       _hover={{ borderColor: "gray.600", bg: "gray.50" }}
