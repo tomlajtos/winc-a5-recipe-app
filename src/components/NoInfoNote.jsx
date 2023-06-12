2f73dee802fd60b63edec99b3ccb1a753c3161f5
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-12 23:37:52 +0200

    Add component to show if there is no data to spec. info

---
+++ src/components/NoInfoNote.jsx
@@ -0,0 +1,19 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const NoInfoNote = ({ category }) => {
+  return (
+    <Box>
+      <Text display={"inline"} mr={2}>
+        -
+      </Text>
+      <Text
+        display={"inline"}
+        fontSize={"sm"}
+        fontStyle={"italic"}
+        textColor={"gray.500"}
+      >
+        (There's no information on '{category}')
+      </Text>
+    </Box>
+  );
+};
