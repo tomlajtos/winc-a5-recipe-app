869c989363e48f1e9a6995b083453a10ef100ede
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-15 19:02:50 +0200

    fix(styles): Make styles more responsive

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -7,9 +7,14 @@ import { useState } from "react";
 export const RecipeListPage = () => {
   const recipes = data.hits.map((hit) => hit.recipe);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
-  console.log(selectedRecipe);
   return (
-    <Wrap minH="100vh" minW={"100%"} flexDir="column" bg={"gray.200"}>
+    <Wrap
+      minH="100vh"
+      w={"100%"}
+      minW={"220px"}
+      flexDir="column"
+      bg={"gray.50"}
+    >
       {selectedRecipe ? (
         <RecipePage
           recipe={selectedRecipe}
@@ -19,8 +24,14 @@ export const RecipeListPage = () => {
         <>
           <Heading
             pt={8}
+            textAlign={"center"}
             textTransform={"uppercase"}
-            fontSize={["4xl", null, "5xl"]}
+            textColor={"white"}
+            sx={{
+              textShadow:
+                "1px 0 0 #34053A, -1px 0 0 #34053A, 0 1px 0 #34053A, 0 -1px 0 #34053A",
+            }}
+            fontSize={["5xl", null, "7xl", null, null, "8xl"]}
           >
             recipe app
           </Heading>
