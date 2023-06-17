effa17a4f3b0be3a2e1952408060939b7e90495a
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-17 16:51:52 +0200

    refactor(App): restructure component-tree
    Move <RecipePage/>, top level <Wrap> from  <RecipeListPage/> to App
    Declare useState for "selectedRecipe" in <App/>
    Declare "recipes" in <App/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import { RecipeListPage } from "./pages/RecipeListPage";
+import { Wrap } from "@chakra-ui/react";
+import { RecipePage } from "./pages/RecipePage";
+import { data } from "./utils/data";
+import { useState } from "react";
 
 function App() {
+  const recipes = data.hits.map((hit) => hit.recipe);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
   return (
-    <>
-      <RecipeListPage />
-    </>
+    <Wrap
+      minH="100vh"
+      w={"100%"}
+      minW={"220px"}
+      flexDir="column"
+      justify={"center"}
+      bg={"gray.50"}
+    >
+      {selectedRecipe ? (
+        <RecipePage
+          recipe={selectedRecipe}
+          handleButtonClick={setSelectedRecipe}
+        />
+      ) : (
+        <RecipeListPage recipes={recipes} handleClick={setSelectedRecipe} />
+      )}
+    </Wrap>
   );
 }
 
--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,43 +1,23 @@
-import { Center as Wrap, Heading } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import { RecipeSearch } from "../components/RecipeSearch";
-import { RecipePage } from "./RecipePage";
-import { data } from "../utils/data";
-import { useState } from "react";
 
-export const RecipeListPage = () => {
-  const recipes = data.hits.map((hit) => hit.recipe);
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
+export const RecipeListPage = ({ recipes, handleClick }) => {
   return (
-    <Wrap
-      minH="100vh"
-      w={"100%"}
-      minW={"220px"}
-      flexDir="column"
-      bg={"gray.50"}
-    >
-      {selectedRecipe ? (
-        <RecipePage
-          recipe={selectedRecipe}
-          handleButtonClick={setSelectedRecipe}
-        />
-      ) : (
-        <>
-          <Heading
-            pt={8}
-            textAlign={"center"}
-            textTransform={"uppercase"}
-            textColor={"white"}
-            sx={{
-              textShadow:
-                "1px 0 0 #34053A, -1px 0 0 #34053A, 0 1px 0 #34053A, 0 -1px 0 #34053A",
-            }}
-            fontSize={["5xl", null, "7xl", null, null, "8xl"]}
-          >
-            recipe app
-          </Heading>
-          <RecipeSearch recipes={recipes} handleClick={setSelectedRecipe} />
-        </>
-      )}
-    </Wrap>
+    <>
+      <Heading
+        pt={8}
+        textAlign={"center"}
+        textTransform={"uppercase"}
+        textColor={"white"}
+        sx={{
+          textShadow:
+            "1px 0 0 #34053A, -1px 0 0 #34053A, 0 1px 0 #34053A, 0 -1px 0 #34053A",
+        }}
+        fontSize={["5xl", null, "7xl", null, null, "8xl"]}
+      >
+        recipe app
+      </Heading>
+      <RecipeSearch recipes={recipes} handleClick={handleClick} />
+    </>
   );
 };
