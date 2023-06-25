b370c83e507a43997c549b70eacdb19428df4169
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-26 01:34:17 +0200

    fix(ButtonChackBox):revert prev fix, return null when unchecked

--- src/components/ui/CustomCheckboxIcon.jsx
+++ src/components/ui/CustomCheckboxIcon.jsx
@@ -29,7 +29,12 @@ export function CustomCheckboxIcon(props) {
       break;
   }
 
-  // isChecked === true returns  an icon with empty "d" because icon border is used as an area separator in "ButtonCheckbox"
+  isIndeterminate
+    ? [
+        "M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm0,19a1.5,1.5,0,1,1,1.5-1.5A1.5,1.5,0,0,1,12,19Zm1.6-6.08a1,1,0,0,0-.6.917,1,1,0,1,1-2,0,3,3,0,0,1,1.8-2.75A2,2,0,1,0,10,9.255a1,1,0,1,1-2,0,4,4,0,1,1,5.6,3.666Z",
+      ]
+    : d;
+
   return (
     <>
       {isChecked ? (
@@ -49,11 +54,7 @@ export function CustomCheckboxIcon(props) {
             />
           )}
         </Icon>
-      ) : (
-        <Icon viewBox="0 0 24 24" {...rest}>
-          <path fill="none" stroke="#ff0000" strokeWidth={"1.5"} d={""} />
-        </Icon>
-      )}
+      ) : null}
     </>
   );
 }
