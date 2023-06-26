ebafdd43b049e502c4be0f4f637547b35145f013
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-27 01:56:42 +0200

    fix(Button): leave highlight in place for accessibility

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,35 +2,7 @@ import { Button as UiButton } from "@chakra-ui/react";
 
 export const Button = ({ handleClick, text, ...props }) => {
   return (
-    <UiButton
-      _focus={{
-        bg: "transparent",
-        boxShadow: "none",
-        WebkitTapHighlightColor: "transparent",
-      }}
-      _focusWithin={{
-        bg: "transparent",
-        boxShadow: "none",
-        WebkitTapHighlightColor: "transparent",
-      }}
-      _focusVisible={{
-        bg: "transparent",
-        boxShadow: "none",
-        WebkitTapHighlightColor: "transparent",
-      }}
-      _highlighted={{
-        bg: "transparent",
-        boxShadow: "none",
-        WebkitTapHighlightColor: "transparent",
-      }}
-      _active={{
-        bg: "transparent",
-        boxShadow: "none",
-        WebkitTapHighlightColor: "transparent",
-      }}
-      onClick={handleClick}
-      {...props}
-    >
+    <UiButton onClick={handleClick} {...props}>
       {text}
     </UiButton>
   );
