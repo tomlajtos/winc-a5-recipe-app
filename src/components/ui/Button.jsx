ed6e4836cf5ecffc2bee21dd8f7ad3c3c722eecf
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-06-26 14:01:35 +0200

    fix(ButtonCheckbox): make highlight color transparent, prevents flickering

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,7 +2,35 @@ import { Button as UiButton } from "@chakra-ui/react";
 
 export const Button = ({ handleClick, text, ...props }) => {
   return (
-    <UiButton onClick={handleClick} {...props}>
+    <UiButton
+      _focus={{
+        bg: "transparent",
+        boxShadow: "none",
+        WebkitTapHighlightColor: "transparent",
+      }}
+      _focusWithin={{
+        bg: "transparent",
+        boxShadow: "none",
+        WebkitTapHighlightColor: "transparent",
+      }}
+      _focusVisible={{
+        bg: "transparent",
+        boxShadow: "none",
+        WebkitTapHighlightColor: "transparent",
+      }}
+      _highlighted={{
+        bg: "transparent",
+        boxShadow: "none",
+        WebkitTapHighlightColor: "transparent",
+      }}
+      _active={{
+        bg: "transparent",
+        boxShadow: "none",
+        WebkitTapHighlightColor: "transparent",
+      }}
+      onClick={handleClick}
+      {...props}
+    >
       {text}
     </UiButton>
   );
