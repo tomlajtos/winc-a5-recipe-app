09a7bb3cffd03d115a0c200115dce11172eb9a67
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-07-19 09:52:55 +0200

    refactor(searchFunctions): Tidy up code and add comments

--- src/utils/searchFunctions.js
+++ src/utils/searchFunctions.js
@@ -1,18 +1,19 @@
 /**
  * Check if recipe have matching pattern(from search field) in its label(title)
+ *
  * @param {object} recipe
  * @param {string} term
- * @return{boolean}
+ * @returns {boolean}
  */
-const isMatchingLabel = (recipe, term) => {
-  return recipe.label.toLowerCase().includes(term.toLowerCase());
-};
+const isMatchingLabel = (recipe, term) =>
+  recipe.label.toLowerCase().includes(term.toLowerCase());
 
 /**
  * Check if recipe have matching pattern(from search field) in its health-labels(title)
+ *
  * @param {object} recipe
  * @param {string} term
- * @return{boolean}
+ * @returns {boolean}
  */
 const isMatchingHealthLabel = (recipe, term) =>
   recipe.healthLabels
@@ -21,9 +22,10 @@ const isMatchingHealthLabel = (recipe, term) =>
 
 /**
  * Return recipes which have matching patterns(from search field) in their labels(titles) and/or health-labels
+ *
  * @param {array<object>} recipes
  * @param {string} term
- * @returns {array<object>}
+ * @returns {array<object>} recipes-filtered
  */
 const filterRecipes = (recipes, term) => {
   return recipes.filter(
@@ -34,8 +36,9 @@ const filterRecipes = (recipes, term) => {
 
 /**
  * Sort recipes alpahabetically by their title
+ *
  * @param {array<object>} recipes
- * @returns {array<object>}
+ * @returns {array<object>} recipes-sorted
  * */
 const sortRecipesByTitle = (recipes) => {
   return recipes.sort((a, b) =>
@@ -44,43 +47,13 @@ const sortRecipesByTitle = (recipes) => {
 };
 
 /**
- * Flatten a nested array comprising of the results (recipes) of different searches and remove duplicate elements
+ * Flatten a nested array comprising the results of different searches (matched recipes)
+ * and remove duplicate elements
+ *
  * @param {array<object>} recipes
  * @param {array<array(object)>} matches
- * @returns {array<object>}
+ * @returns {array<object>} matches-combined
  * */
-// const combineMatches = (recipes, matches, c) => {
-//   // console.log("***Calling combineMatches***");
-//   // console.log("MATCHES IN COMBINE:", matches, matches.length, c);
-//   // return (
-//   // sortRecipesByTitle(
-//   // sort arrays by size and combine them
-//   let sortedMatches = matches.sort((a, b) => a.length - b.length);
-//   // console.log("SRTD M:", sortedMatches);
-//   let isAllOrigLen = sortedMatches.every((item) => item.length === 20);
-//   // console.log("every match item is 20 long:", isAllOrigLen);
-//   // console.log(
-//   // "test (t):",
-//   // [1, 3, 5].every((i) => i < 6)
-//   // );
-//   let y = isAllOrigLen
-//     ? // if all matches are the same as original recipes array
-//       // matches[0]
-//       recipes
-//     : // leave out matches that are the same as original
-//       matches
-//         .filter((item) => item.length && item.length < recipes.length)
-//         .reduce((res, item) => res.concat(item), []);
-//   // );
-//   // console.log("YYY:", y);
-//   let x = sortRecipesByTitle(y);
-//   // remove duplicates from array - elemets needed to be sorted alphabetically first
-//   // console.log("XXX", x);
-//   return x.filter((recipe, index, arr) =>
-//     arr.length === index + 1 ? recipe : recipe.label !== arr[index + 1].label
-//   );
-//   // );
-// };
 const combineMatches = (recipes, matches) => {
   return (
     sortRecipesByTitle(
@@ -90,8 +63,7 @@ const combineMatches = (recipes, matches) => {
         .every((item) => {
           return item.length === recipes.length;
         })
-        ? // if all matches are the same as original recipes array
-          // matches[0]
+        ? // if all matches are the same as original recipes array, return original
           recipes
         : // leave out matches that are the same as original
           matches
@@ -108,35 +80,31 @@ const combineMatches = (recipes, matches) => {
 };
 
 /**
- * Filter recipes by individual serch terms, result is the combination of all the separate results
+ * Filter recipes by individual serch terms, the result is the combination of all the separate results
  * sorted alphabetically by title
  *
  * @param {array<string>} terms
  * @param {array<object>} recipes
- * @returns {array<object>}
+ * @returns {array<object>} recipes-filtered
  */
-const filterBySimpleTerms = (terms, recipes, c) => {
-  // console.log("***calling filterBySimpleTerms***");
-  // console.log("TERMS IN SIMPLE:", terms, c);
-
+const filterBySimpleTerms = (terms, recipes) => {
   const matches = terms
     .map((term) => filterRecipes(recipes, term))
     // do not return empty arrays
     .filter((i) => i.length);
 
-  // console.log("MATCHES IN FILTER BY SIMPLET: ", matches, c);
-  return matches ? combineMatches(recipes, matches, "simple", c) : [];
+  return matches ? combineMatches(recipes, matches) : [];
 };
 
 /**
  * Filter recipes by complex search terms (key-words combined with "+"), can be multiple complex terms separated by comma
  * uses recursion to map nested arrays
+ *
  * @param {array<array(sring)>} terms
  * @param {array<object>} recipes
- * @returns {array<object>}
+ * @returns {array<object>} recipes-filtered
  */
 const filterByComplexTerms = (terms, recipes) => {
-  console.log("TERMS_CX:", terms);
   const mapViaTerms = (terms) => {
     let matches = [...recipes];
 
@@ -149,60 +117,59 @@ const filterByComplexTerms = (terms, recipes) => {
   const mapViaNestedTs = (fn, [head, ...tail]) => {
     return head === undefined ? [] : [...fn(head), ...mapViaNestedTs(fn, tail)];
   };
+
   return mapViaNestedTs(mapViaTerms, terms);
 };
 
 /**
- * return matching recipes based on set filters
+ * Return matching recipes based on search input
+ *
+ * @param {array<object>} recipes
+ * @param {string} searchField
+ * @returns {array<object>} combinedSearchMatches
  * */
-export const matchRecipesToFilters = (recipes, filters) => {
+const matchRecipesToSearch = (recipes, searchField) => {
+  // split user input in serchfield by selected special characters and white-space,
+  // hyphen is not included since it can be found in health labels.
+  const searchTerms = searchField.split(/[\s,/\\]+/g);
+
+  // serach key-words which are not combined with a '+' character,
+  const simpleTerms = searchTerms.filter((term) => !term.includes("+"));
+
+  // serach key-words which are combined with a '+' character (to narrow down results)
+  // complexTerms is allways a nested array
+  const complexTerms = searchTerms
+    .filter((term) => term.includes("+"))
+    .map((term) => term.split("+"));
+
+  const complexSearchMatches = filterByComplexTerms(complexTerms, recipes);
+  const simpleSearchMatches = filterBySimpleTerms(simpleTerms, recipes);
+  const combinedSearchMatches = combineMatches(recipes, [
+    complexSearchMatches,
+    simpleSearchMatches,
+  ]);
+
+  return combinedSearchMatches;
+};
+
+/**
+ * Return matching recipes based on set filter,
+ * It's output is feeded to search as a starting point
+ *
+ * @param {array<object>} recipes
+ * @param {array<object>} filters
+ * @returns {array<object>} recipes-filtered
+ * */
+const matchRecipesToFilters = (recipes, filters) => {
   // array of filter IDs <string>
   const filterTerms = filters.reduce(
     (res, filter) => (filter.isSelected ? res.concat(filter.id) : res),
     []
   );
 
-  console.log("FT", filterTerms);
-  console.log("filter matches:", filterBySimpleTerms(filterTerms, recipes));
-
   return filters
-    ? filterBySimpleTerms(filterTerms, recipes, "filt")
+    ? filterBySimpleTerms(filterTerms, recipes)
     : sortRecipesByTitle(recipes);
 };
 
-/**
- * return matching recipes based on search input
- * */
-export const findMatchingRecipes = (recipes, searchField) => {
-  // split user input in serchfield by below special characters and white-space,
-  // hyphen is not included since it can be found in health labels.
-  const searchTerms = searchField.split(/[\s,/\\]+/g);
-
-  // serach key-words which are not combined with a '+' character, aka. there is no intent to narrow down search results
-  const simpleTerms = searchTerms.filter((term) => !term.includes("+"));
-
-  // allways a nested array
-  const complexTerms = searchTerms
-    .filter((term) => term.includes("+"))
-    .map((term) => term.split("+"));
-
-  // console.log("terms for search:", complexTerms, simpleTerms);
-  const complexSearchMatches = filterByComplexTerms(complexTerms, recipes);
-
-  const simpleSearchMatches = filterBySimpleTerms(simpleTerms, recipes, "find");
-
-  console.log(
-    "before combining complex: ",
-    complexSearchMatches,
-    "before combining simple:",
-    simpleSearchMatches
-  );
-  const combinedSearchMatches = combineMatches(
-    recipes,
-    [complexSearchMatches, simpleSearchMatches],
-    "FINAL in find"
-  );
-
-  console.log(combinedSearchMatches);
-  return combinedSearchMatches;
-};
+export { matchRecipesToSearch, matchRecipesToFilters };
